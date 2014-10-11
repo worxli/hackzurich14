60b68690b04fb3d32c3eef3162a112f51b5b4036
vogelfr <vogelfr@student.ethz.ch>
2014-10-12 00:38:55 +0200

    Starting with webform to enter data to DB

--- webserver/index.php
+++ webserver/index.php
@@ -14,13 +14,100 @@
         include 'config.php';
     ?>
 
-    <!--<form action="setProfile.php" method="post">
+    <form action="setProfile.php" method="post">
         Name: <input type="text" name="name"><br>
         First Name: <input type="text" name="first_name"><br>
         UID: <input type="text" name="UID"><br>
         UUID: <input type="text" name="UUID"><br>
         <input type="submit">
-    </form>-->
+    </form>
+    
+    <?php
+// define variables and set to empty values
+$nameErr = $first_nameErr = $emailErr = $dobErr = $phoneErr = "";
+$name = $first_name = $dob = $address = $postcode = $city = $land = $email_address = $phone_number = $facebook = $tiwtter = $linkedin = $xing = $UUID = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   if (empty($_POST["name"])) {
+     $nameErr = "Name is required";
+   } else {
+     $name = test_input($_POST["name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+       $nameErr = "Only letters and white space allowed"; 
+     }
+   }
+    
+    if (empty($_POST["first_name"])) {
+     $nameErr = "First Name is required";
+   } else {
+     $name = test_input($_POST["first_name"]);
+     // check if name only contains letters and whitespace
+     if (!preg_match("/^[a-zA-Z ]*$/",$first_name)) {
+       $first_nameErr = "Only letters and white space allowed"; 
+     }
+   }
+    
+    if (empty($_POST["dob"])) {
+        $dob = "";
+    } else {
+        $dob = test_input($_POST["dob"]);
+    }
+    
+    
+   
+   if (!empty($_POST["email"])) {
+     $email = test_input($_POST["email"]);
+     // check if e-mail address is well-formed
+     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $emailErr = "Invalid email format"; 
+     } else {
+         $email = "";
+     }
+   }
+     
+   if (empty($_POST["website"])) {
+     $website = "";
+   } else {
+     $website = test_input($_POST["website"]);
+     // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+       $websiteErr = "Invalid URL"; 
+     }
+   }
+
+   
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<p><span class="error">* required field.</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+   Name: <input type="text" name="name" value="<?php echo $name;?>">
+   <span class="error">* <?php echo $nameErr;?></span>
+   <br><br>
+   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+   <span class="error">* <?php echo $emailErr;?></span>
+   <br><br>
+   Website: <input type="text" name="website" value="<?php echo $website;?>">
+   <span class="error"><?php echo $websiteErr;?></span>
+   <br><br>
+   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+   <br><br>
+   Gender:
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?>  value="female">Female
+   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?>  value="male">Male
+   <span class="error">* <?php echo $genderErr;?></span>
+   <br><br>
+   <input type="submit" name="submit" value="Submit"> 
+</form>
  
 </body>
 </html>
