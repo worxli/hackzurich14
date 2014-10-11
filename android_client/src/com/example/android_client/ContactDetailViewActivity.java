7076134cfc9652daad472a6d0e717786527e58eb
vogelfr <vogelfr@student.ethz.ch>
2014-10-11 15:20:34 +0200

    integrated

---
+++ android_client/src/com/example/android_client/ContactDetailViewActivity.java
@@ -0,0 +1,65 @@
+package com.example.android_client;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class ContactDetailViewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_detail_view);
+		Intent myIntent = getIntent();
+		String uuid = myIntent.getStringExtra("uuid");
+		
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost("http://hackzurich14.worx.li/getByUUID.php");
+
+		List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+		pairs.add(new BasicNameValuePair("UUID", uuid));
+		try {
+			post.setEntity(new UrlEncodedFormEntity(pairs));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			HttpResponse response = client.execute(post);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		//HashMap<String, String> data = card.getAll();
+		
+		ListView listview = (ListView) findViewById(R.id.listView1);
+		
+		
+	}
+
+}
